e3277dba9b8e36b45ce8c0a4211b38e7f271bd4e
rajat bansal <rajatbansal28082003@gmail.com>
2024-12-15 18:38:26 +0530

    Service: RabbitMQ Broker

--- gateway/src/index.ts
+++ gateway/src/index.ts
@@ -32,8 +32,20 @@ const authServiceBreaker = new CircuitBreaker(
     resetTimeout: 30000,
   }
 );
+const scoreServiceBreaker = new CircuitBreaker(
+  (req: Request, res: Response) =>
+    createProxyMiddleware({
+      target: "http://localhost:5004",
+      changeOrigin: false,
+    })(req, res),
+  {
+    timeout: 3000,
+    errorThresholdPercentage: 50,
+    resetTimeout: 30000,
+  }
+);
 
-const roomServiceBreaker = new CircuitBreaker(
+const homeServiceBreaker = new CircuitBreaker(
   (req: Request, res: Response) =>
     createProxyMiddleware({
       target: "http://localhost:5002",
@@ -56,11 +68,29 @@ app.use(
   "/home",
   AuthMiddleware,
   (req: Request, res: Response, next: NextFunction) => {
-    roomServiceBreaker.fire(req, res).catch((err) => {
+    homeServiceBreaker.fire(req, res).catch((err) => {
       res.status(503).json({ error: "Room service is unavailable" });
     });
   }
 );
+app.use(
+  "/score",
+  AuthMiddleware,
+  (req: Request, res: Response, next: NextFunction) => {
+    scoreServiceBreaker.fire(req, res).catch((err) => {
+      res.status(503).json({ error: "score service is unavailable" });
+    });
+  }
+);
+// app.use(
+//   "/home",
+//   AuthMiddleware,
+//   (req: Request, res: Response, next: NextFunction) => {
+//     roomServiceBreaker.fire(req, res).catch((err) => {
+//       res.status(503).json({ error: "Room service is unavailable" });
+//     });
+//   }
+// );
 
 app.listen(process.env.PORT, () => {
   console.log("API Gateway is running on port 5000");
